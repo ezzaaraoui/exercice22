0d04ef9b699255bb741a2a4441692404793d881b
ayoub ezzaaraoui <146847154+ezzaaraoui@users.noreply.github.com>
2023-10-07 22:25:30 +0200

    Add files via upload

---
+++ exercice22.py
@@ -0,0 +1,13 @@
+nombre=int(input("donner un nombre SLV :"))
+
+c=nombre
+while(c!=nombre+10):
+    c=c+1
+    print(c)
+
+#Do while 
+while True:
+    c=c+1
+    print(c)
+    if c==nombre+10 :
+        break
